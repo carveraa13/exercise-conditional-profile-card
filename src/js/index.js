54cff0df8d6b67ad3686e115ebe30fff7fdad16f
Carlos Vera A <carveraa13@gmail.com>
2021-02-09 02:15:35 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -28,12 +28,15 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+    let pname = variables.name ? variables.name : "Lucy";
+    let plastname = variables.plastname ? variables.plastname : "Boliet";
+    let prole = variables.role;
+    let pplace = variables.place + " " + variables.country;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${pname} ${plastname}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
